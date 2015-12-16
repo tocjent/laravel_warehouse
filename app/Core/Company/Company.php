2542a5121d19a5d76f6bc18e5aef9f5ed26a0a15
tocjent <bobrowicz.michal@gmail.com>
2015-12-16 14:19:56 +0100

    initial commit

---
+++ app/Core/Company/Company.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Core\Company;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    //
+}
---
+++ database/migrations/2015_12_16_131344_create_addresses__table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('adresses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('city');
+            $table->string('street');
+            $table->string('postalCode');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('adresses');
+    }
+}
